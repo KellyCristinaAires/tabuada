a91622badcb404f30e26608019fa35d32eb8bf4b
KellyCristinaAires <k.woys@hotmail.com>
2020-11-21 17:13:05 -0300

    atividade tabuada

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             string numero1;
+            int numero;
+            Console.Write ("Digite um numero: " );
+			numero1=Console.ReadLine();
+            numero=int.Parse(numero1);
+			//Console.ReadKey(true);
+	
+		
+		//public static void tabuada(int numero)
+		
+			for (int x = 1; x <= 10; x++)
+			{
+				Console.WriteLine(numero1 + " * " + x + " = " + numero*x);
+            }
+        }
+    }
+}
